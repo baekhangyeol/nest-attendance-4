fde4f65b9074d41b5542cac2d29f22243acedd42
HanGyeol Baek <76465887+baekhangyeol@users.noreply.github.com>
2024-04-02 12:16:39 +0900

    feat: User, Attendace 엔티티, Status Enum 추가

---
+++ src/attendance/entities/attendance.entity.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from './user.entity';
+import { AttendanceStatusEnum } from './attendance-status.enum';
+
+@Entity()
+export class Attendance {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  attendanceTime: Date;
+
+  @Column({
+    type: 'enum',
+    enum: AttendanceStatusEnum,
+  })
+  status: AttendanceStatusEnum;
+
+  @OneToOne(() => User)
+  user: User;
+}
